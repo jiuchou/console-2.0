4263b0413b4f91366222122215977bad9655faf4
jiuchou <45912997+jiuchou@users.noreply.github.com>
2019-09-20 14:33:21 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import Main from './Main.vue'
+import router from './router'
+import store from './store'
+
+import './permission'
 
 Vue.config.productionTip = false
 
-import Main from './Main.vue'
-import router from './router'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// global css
+import '@/styles/index.scss'
+
+import Cookies from 'js-cookie'
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium'
+})
 
 new Vue({
   router,
+  store,
   render: h => h(Main),
 }).$mount('#app')
